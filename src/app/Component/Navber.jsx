ae41467f933437517ce625f8a49f55bb696e1dbb
Danesh Kumar <dk4568537@gmail.com>
2024-09-20 06:29:38 -0700

    update

--- src/app/Component/Navber.jsx
+++ src/app/Component/Navber.jsx
@@ -1,171 +1,159 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
-import Navlogo from "../../../public/images/Navlogo.svg";
+import React from "react";
+import Navlogo from "../../../public/images/LogoNav.svg";
 import Accountlogo from "../../../public/images/Accountlogo.svg";
 import Locationlogo from "../../../public/images/Locationlogo.svg";
 import Worldlogo from "../../../public/images/Worldlogo.svg";
 const Bgssimg = "/images/Dashboard-bg.png";
 import Image from "next/image";
-import { Button } from "@chakra-ui/react";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { HiChevronDown } from "react-icons/hi";
+import { FaUser } from "react-icons/fa";
 
 const Navber = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const handleMenuToggle = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    return (
-        <div className="bg-black bg-cover">
-            <div
-                className={`w-[100%] h-full bg-cover bg-center`}
-                style={{ backgroundImage: `url(${Bgssimg})` }}
-            >
-                <nav className="relative border-gray-200 dark:bg-gray-900">
-                    <div className="w-full sm:max-w-screen-xl flex items-center justify-between mx-auto p-4">
-                        <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                            <span className="self-center sm:text-2xl font-semibold whitespace-nowrap text-white">
-                                Rent Hire
-                            </span>
-                            <Image
-                                src={Navlogo}
-                                className="h-6 bg-white rounded-full"
-                                alt="Rent Hire Logo"
-                            />
-                        </Link>
-
-                        <div className="flex md:order-2 space-x-3 rtl:space-x-reverse">
-                            <button
-                                type="button"
-                                className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                            >
-                                Get Started
-                            </button>
-                            <button
-                                onClick={handleMenuToggle}
-                                type="button"
-                                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                aria-controls="navbar-cta"
-                                aria-expanded={isMenuOpen ? "true" : "false"}
-                            >
-                                <span className="sr-only">Open main menu</span>
-                                <svg
-                                    className="w-5 h-5"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 17 14"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M1 1h15M1 7h15M1 13h15"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-
-                        <div
-                            className={`${isMenuOpen ? "absolute left-0 top-5 w-full" : "hidden"
-                                } mt-11 md:mt-0 md:relative md:flex md:w-auto md:order-1`}
-                            id="navbar-cta"
-                        >
-                            <ul className="flex flex-col font-medium border rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block text-lg font-semibold py-2 px-3 md:p-0 text-white hover:bg-green-500 rounded md:bg-transparent md:text-white md:dark:text-blue-500"
-                                        aria-current="page"
-                                    >
-                                        Home
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block text-lg font-semibold py-2 px-3 md:p-0 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        Product
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block text-lg font-semibold py-2 px-3 md:p-0 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        About us
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-
-
-                <div className="flex justify-center items-center min-h-screen text-center px-4">
-                    <div className="max-w-2xl text-white">
-                        <h3 className="mb-4 font-semibold text-xs sm:text-3xl md:text-2xl">
-                            Streamline Your Rental Business with Go-Hire.com
-                        </h3>
-                        <h1 className="mb-6 text-sm sm:text-3xl md:text-3xl font-bold">
-                            Connecting Sellers and Customers Globally with Efficient Rental
-                            Management.
-                        </h1>
-                        <p className="font-semibold text-xs sm:text-lg md:text-lg">
-                            Lorem ipsum dolor sit amet consectetur. Luctus duis lectus risus
-                            augue suspendisse interdum felis. Hac eu nulla quam mauris urna
-                            vitae amet elit. Rutrum tortor ut porta magna praesent eget mauris
-                            lectus velit. Sed mattis faucibus imperdiet pretium egestas nunc
-                            pretium eu condimentum.
-                        </p>
-                        <Button colorScheme="green" className="mt-5">
-                            Get Started Today
-                        </Button>
-                    </div>
-                </div>
-
-                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 pb-5">
-                    <div className="flex justify-center items-center gap-5">
-                        <div className="flex gap-5 items-center text-lg font-bold">
-                            <div className="border p-3 bg-white border-black rounded-full">
-                                <Image className="sm:w-8 sm:h-8" src={Accountlogo} alt="" />
-                            </div>
-                            <div className="text-white flex flex-col">
-                                <p>1K+</p>
-                                <p>Users</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-center items-center gap-5">
-                        <div className="flex gap-5 items-center text-lg font-bold">
-                            <div className="border p-3 bg-white border-black rounded-full">
-                                <Image className="sm:w-8 sm:h-8" src={Locationlogo} alt="" />
-                            </div>
-                            <div className="text-white flex flex-col">
-                                <p>50K+</p>
-                                <p>Location</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-center items-center gap-5">
-                        <div className="flex gap-5 items-center text-lg font-bold">
-                            <div className="border p-3 bg-white border-black rounded-full">
-                                <Image className="sm:w-8 sm:h-8" src={Worldlogo} alt="" />
-                            </div>
-                            <div className="text-white flex flex-col">
-                                <p>50K+</p>
-                                <p>Server</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="bg-black bg-cover">
+      <div
+        className="w-[100%] h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${Bgssimg})` }}
+      >
+        <nav className="border-gray-200 dark:bg-gray-900">
+          <div className="w-full sm:max-w-screen-xl flex items-center justify-end sm:gap-[55px] md:gap-[338px] mx-auto p-4">
+            <div className="md:hidden">
+              <Image src={Navlogo} className="h-[58.56px] w-[73.5px]" alt="Rent Hire Logo" />
             </div>
+
+            <div className="flex md:order-2 space-x-3 rtl:space-x-reverse">
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs sm:text-sm px-5 py-2.5 text-center"
+              >
+                Get Started
+              </button>
+              <Menu>
+                <MenuButton
+                  colorScheme="white"
+                  variant="ghost"
+                  border={"none"}
+                  color={"white"}
+                  as={Button}
+                  rightIcon={<HiChevronDown />}
+                >
+                  English
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Option One</MenuItem>
+                  <MenuItem>Option two</MenuItem>
+                  <MenuItem>Option Three</MenuItem>
+                  <MenuItem>Option Four</MenuItem>
+                  <MenuItem>Option Five</MenuItem>
+                </MenuList>
+              </Menu>
+              <p className="flex justify-center items-center text-center text-white">
+                <FaUser />
+              </p>
+            </div>
+
+            <div className="hidden md:flex md:order-1">
+              <Image src={Navlogo} className="sm:h-[58.56px] sm:w-[73.5px]" alt="Rent Hire Logo" />
+            </div>
+          </div>
+        </nav>
+
+        <div className="flex justify-center items-center">
+          <ul className="flex md:flex-row font-medium bg-black md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="#"
+                className="block text-lg font-semibold py-2 px-3 md:p-0 text-white hover:text-green-500 rounded md:bg-transparent md:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="block text-lg font-semibold py-2 px-3 md:p-0 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Product
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="block text-lg font-semibold py-2 px-3 md:p-0 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                About us
+              </Link>
+            </li>
+          </ul>
         </div>
-    );
+
+        <div className="flex justify-center items-center min-h-screen text-center px-4">
+          <div className="max-w-2xl text-white">
+            <h3 className="mb-4 font-semibold text-xs sm:text-3xl md:text-2xl">
+              Streamline Your Rental Business with Go-Hire.com
+            </h3>
+            <h1 className="mb-6 text-sm sm:text-3xl md:text-3xl font-bold">
+              Connecting Sellers and Customers Globally with Efficient Rental
+              Management.
+            </h1>
+            <p className="font-semibold text-xs sm:text-lg md:text-lg">
+              Lorem ipsum dolor sit amet consectetur. Luctus duis lectus risus
+              augue suspendisse interdum felis. Hac eu nulla quam mauris urna
+              vitae amet elit. Rutrum tortor ut porta magna praesent eget mauris
+              lectus velit. Sed mattis faucibus imperdiet pretium egestas nunc
+              pretium eu condimentum.
+            </p>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r mt-5 from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 pb-5 gap-6">
+          <div className="flex justify-center    items-center gap-5">
+            <div className="flex gap-5 items-center text-lg font-bold">
+              <div className="border p-3 bg-white border-black rounded-full">
+                <Image className="sm:w-8 sm:h-8" src={Accountlogo} alt="Account" />
+              </div>
+              <div className="text-white flex flex-col">
+                <p>1K+</p>
+                <p>Users</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <div className="flex gap-5 items-center text-lg font-bold">
+              <div className="border p-3 bg-white border-black rounded-full">
+                <Image className="sm:w-8 sm:h-8" src={Locationlogo} alt="Location" />
+              </div>
+              <div className="text-white flex flex-col">
+                <p>50K+</p>
+                <p>Locations</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <div className="flex gap-5 items-center text-lg font-bold">
+              <div className="border p-3 bg-white border-black rounded-full">
+                <Image className="sm:w-8 sm:h-8" src={Worldlogo} alt="World" />
+              </div>
+              <div className="text-white flex flex-col">
+                <p>50K+</p>
+                <p>Servers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navber;
--- src/app/Component/QuickBooks.jsx
+++ src/app/Component/QuickBooks.jsx
@@ -1,53 +1,99 @@
 "use client"
 import { Button } from '@chakra-ui/react';
 import Image from 'next/image';
-import React, { useState } from 'react';
-import videoimg from '../../../public/images/Video-img.svg'
+import React, { useEffect, useState } from 'react';
+import videoimg from '../../../public/images/Video-img.svg';
+
 const QuickBooks = () => {
   const [activeContent, setActiveContent] = useState('');
+  const [scrollValue, setScrollValue] = useState(0);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollValue(window.scrollY); // For vertical scroll
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const handleButtonClick = (content) => {
     setActiveContent(content);
   };
 
+  // Conditionally set width based on scrollValue and apply smooth transition
+  const imageWidthClass = scrollValue >= 800 ? `w-[650px]` : scrollValue >= 620  ?'w-[780px]': scrollValue >= 520  ?'w-[900px]': 'w-full';
+
+
   return (
-    <div className=' bg-black pb-5'>
+    <div className='bg-black pb-5'>
       <div className="flex justify-center flex-wrap items-center gap-2 sm:gap-14 pb-5 pt-8">
-        <Button bgColor={"gray"} color={'white'} onClick={() => handleButtonClick('QuickBooks')}>
+        <Button colorScheme='whiteAlpha' variant='ghost' onClick={() => handleButtonClick('QuickBooks')}>
           QuickBooks
         </Button>
-        <Button  bgColor={"gray"} color={'white'} onClick={() => handleButtonClick('Chat')}>
+        <Button colorScheme='whiteAlpha' variant='ghost' onClick={() => handleButtonClick('Chat')}>
           Chat
         </Button>
-        <Button  bgColor={"gray"} color={'white'} onClick={() => handleButtonClick('Product')}>
+        <Button colorScheme='whiteAlpha' variant='ghost' onClick={() => handleButtonClick('Product')}>
           Product
         </Button>
-        <Button  bgColor={"gray"} color={'white'} onClick={() => handleButtonClick('Vendor Registration')}>
+        <Button colorScheme='whiteAlpha' variant='ghost' onClick={() => handleButtonClick('Vendor Registration')}>
           Vendor Registration
+        </Button>
+        <Button colorScheme='whiteAlpha' variant='ghost' onClick={() => handleButtonClick('Xero')}>
+          Xero
         </Button>
       </div>
 
       {/* Conditionally render content based on active button */}
       <div className="w-full py-5">
-        {activeContent === 'QuickBooks' && (
-          <div>
-            <Image className=' sm:h-[600px] w-full' src={videoimg} alt="" />
+        {activeContent == "QuickBooks" && (
+          <div className=' flex justify-center items-center'>
+            <Image
+              className={`sm:h-[600px] ${imageWidthClass} transition-all ease-in-out duration-700`} 
+              src={videoimg} 
+              alt="" 
+            />
           </div>
         )}
-        {activeContent === 'Chat' && (
-          <div>
-            <Image className=' sm:h-[600px] w-full' src={videoimg} alt="" />
+        {activeContent == "Chat" && (
+          <div className=' flex justify-center items-center'>
+            <Image
+              className={`sm:h-[600px] ${imageWidthClass} transition-all ease-in-out duration-700`} 
+              src={videoimg} 
+              alt="" 
+            />
           </div>
         )}
-        {activeContent === 'Product' && (
-          <div>
-            <Image className=' sm:h-[600px] w-full' src={videoimg} alt="" />
+        {activeContent == "Product" && (
+          <div className=' flex justify-center items-center'>
+            <Image
+              className={`sm:h-[600px] ${imageWidthClass} transition-all ease-in-out duration-700`} 
+              src={videoimg} 
+              alt="" 
+            />
           </div>
         )}
-        {activeContent === 'Vendor Registration' && (
-          <div>
-            <Image className=' sm:h-[600px] w-full' src={videoimg} alt="" />
+        {activeContent == "Vendor Registration" && (
+          <div className=' flex justify-center items-center'>
+            <Image
+              className={`sm:h-[600px] ${imageWidthClass} transition-all ease-in-out duration-700`} 
+              src={videoimg} 
+              alt="" 
+            />
+          </div>
+        )}
+        {activeContent == " Xero" && (
+          <div className=' flex justify-center items-center'>
+            <Image
+              className={`sm:h-[600px] ${imageWidthClass} transition-all ease-in-out duration-700`} 
+              src={videoimg} 
+              alt="" 
+            />
           </div>
         )}
       </div>
